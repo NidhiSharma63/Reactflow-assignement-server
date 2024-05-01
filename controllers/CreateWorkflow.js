bcb21d93a7e753664e6891a9f7b388b7754839c4
nidhisharma63 <nidhisharma639593@gmail.com>
2024-05-01 19:32:27 +0530

    validation added for not having start and end more than one

--- controllers/CreateWorkflow.js
+++ controllers/CreateWorkflow.js
@@ -4,6 +4,20 @@ const createWorkFlow = async (req, res, next) => {
   try {
     const { userId, workFlowSequence, workFlowId } = req.body;
     if (!userId || !workFlowSequence) throw new Error("Missing Data");
+
+    /**
+     * check if workFlow sequence includes start and end more than one times
+     */
+    const isStartIncludeMoreThanOne = workFlowSequence.filter((step) => step === "Start").length > 1;
+    const isEndIncludeMoreThanOne = workFlowSequence.filter((step) => step === "End").length > 1;
+
+    if (isStartIncludeMoreThanOne) {
+      throw new Error("Start should be included only once");
+    }
+
+    if (isEndIncludeMoreThanOne) {
+      throw new Error("End should be included only once");
+    }
     const workFlow = new WorkFlow({
       userId,
       workFlowSequence,
--- utils/Errorhandle.js
+++ utils/Errorhandle.js
@@ -3,7 +3,8 @@ const errorHandle = (error, _req, res, next) => {
   if (
     error.message.includes("Missing") ||
     error.message.includes("already exists") ||
-    error.message.includes("not found")
+    error.message.includes("not found") ||
+    error.message.includes("included")
   ) {
     return res.status(400).send(error.message);
   }
