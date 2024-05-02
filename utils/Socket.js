a22d69da506f7982ae647f02905d2da5a3b76a3c
nidhisharma63 <nidhisharma639593@gmail.com>
2024-05-02 15:18:16 +0530

    all cors origin allowed

--- utils/Socket.js
+++ utils/Socket.js
@@ -26,7 +26,7 @@ app.use(errorHandle);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: false,
   },
